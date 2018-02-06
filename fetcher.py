453a1b334b1cac82b6b783c91bfd75226744f8a5
Xin Lai <laixin456@163.com>
2018-02-06 09:49:09 +0800

    adding utils class fetcher used to get webpages and hand it to crawler

---
+++ fetcher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+### class used to obtain context from the network
+import requests,urllib
+
+class fetcher(object):
+	"""class used to obtain context from the network"""
+	def __init__(self, _proxy = None):
+		super(fetcher, self).__init__()
+		self._proxy = _proxy
+	@staticmethod
+	def fetch(_url, _proxy = None):
+		if _proxy is None:
+			return requests.get(_url)
+		elif type(_proxy) is str and _proxy == "system":
+			return requests.get(_url,proxies=urllib.getproxies())
+		elif type(_proxy) is dict and _proxy.has_key("http"):
+			proxy = dict()
+			for protocal in "http" "https" "ftp":
+				try:
+					proxy[protocal] = _proxy.get(protocal)
+				except:
+					proxy[protocal] = _proxy.get("http")
+			return requests.get(_url,proxies=proxy).close()
+
+if __name__ == '__main__':
+	page = fetcher.fetch("http://www.baidu.com")
+	print page.status_code, page.reason
+	print 'time used',page.elapsed
