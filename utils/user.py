0673a5c499e67ec04b5e72bbb4b0e9f7486653e9
Xin Lai <laixin456@163.com>
2018-02-10 10:03:57 +0800

    fix decode errors when reading chinese characters

--- utils/user.py
+++ utils/user.py
@@ -3,24 +3,27 @@
 
 import ConfigParser,io
 import os.path
+import sys
 
-class user(object):
-    """docstring for user"""
-    INI_FILE = """[Heweather]
+codetype = sys.getfilesystemencoding()
+INI_FILE = """[Heweather]
 location = 
 key = 
 """
+
+class user(object):
+    """docstring for user"""
     def __init__(self):
         super(user, self).__init__()
         self.ok = True
         if not os.path.isfile("..\config.ini"):
             f = open("..\config.ini","w")
-            f.writelines(self.INI_FILE)
+            f.writelines(INI_FILE)
             f.close()
             self.ok = False
             #raise Exception("config file not exists, automatically created. Please fill the proper values")
         with open("..\config.ini") as f:
-            myconfig = f.read()
+            myconfig = f.read().decode(codetype).encode("utf-8")
         config = ConfigParser.RawConfigParser(allow_no_value=True)
         config.readfp(io.BytesIO(myconfig))
         self.location = config.get('Heweather','location')
